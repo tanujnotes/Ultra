69bf1e240879d78439b253a452589455c6d65204
Tanuj M <thetanuj@hotmail.com>
2020-12-21 18:04:02 +0530

    Added gesture listener on main layout

--- app/src/main/java/app/olauncher/light/MainActivity.java
+++ app/src/main/java/app/olauncher/light/MainActivity.java
@@ -1,6 +1,7 @@
 package app.olauncher.light;
 
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.LauncherActivityInfo;
@@ -15,6 +16,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,12 +24,26 @@ import static android.view.WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS;
 
 public class MainActivity extends Activity {
     private List<AppModel> appList = new ArrayList<>();
+    private View homeAppsLayout;
+    private View appDrawer;
+
+    @Override
+    public void onBackPressed() {
+        if (appDrawer.getVisibility() == View.VISIBLE) {
+            appDrawer.setVisibility(View.GONE);
+            homeAppsLayout.setVisibility(View.VISIBLE);
+        } else super.onBackPressed();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().addFlags(FLAG_LAYOUT_NO_LIMITS);
+        findViewById(R.id.layout_main).setOnTouchListener(getSwipeGestureListener(this));
+
+        homeAppsLayout = findViewById(R.id.home_apps_layout);
+        appDrawer = findViewById(R.id.app_drawer_layout);
 
         AppAdapter appAdapter = new AppAdapter(this, appList);
         ListView appListView = findViewById(R.id.app_list_view);
@@ -55,6 +71,84 @@ public class MainActivity extends Activity {
         }
     }
 
+    private void showAppList() {
+        homeAppsLayout.setVisibility(View.GONE);
+        appDrawer.setVisibility(View.VISIBLE);
+    }
+
+    @SuppressLint({"WrongConstant", "PrivateApi"})
+    private void expandNotificationDrawer() {
+        try {
+            Object statusBarService = getSystemService("statusbar");
+            Class<?> statusBarManager = Class.forName("android.app.StatusBarManager");
+            Method method = statusBarManager.getMethod("expandNotificationsPanel");
+            method.invoke(statusBarService);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private View.OnTouchListener getSwipeGestureListener(Context context) {
+        return new OnSwipeTouchListener(context) {
+            @Override
+            public void onSwipeLeft() {
+                super.onSwipeLeft();
+//                openSwipeLeftApp();
+            }
+
+            @Override
+            public void onSwipeRight() {
+                super.onSwipeRight();
+//                openSwipeRightApp();
+            }
+
+            @Override
+            public void onSwipeUp() {
+                super.onSwipeUp();
+                showAppList();
+//                showAppList(Constants.FLAG_LAUNCH_APP);
+            }
+
+            @Override
+            public void onSwipeDown() {
+                super.onSwipeDown();
+                expandNotificationDrawer();
+            }
+
+            @Override
+            public void onLongClick() {
+                super.onLongClick();
+//                try {
+//                    findNavController().navigate(R.id.action_mainFragment_to_settingsFragment)
+//                    viewModel.firstOpen(false)
+//                } catch (e: java.lang.Exception) {
+//                }
+            }
+
+            @Override
+            public void onDoubleClick() {
+//                if (prefs.lockModeOn) {
+//                    if (Settings.System.canWrite(requireContext())) {
+//                        requireActivity().runOnUiThread {
+//                            blackOverlay.visibility = View.VISIBLE
+//                            setScreenTimeout()
+//                            hideNavBar()
+//                        }
+//                    } else {
+//                        lockPhone()
+//                    }
+//                }
+                super.onDoubleClick();
+            }
+
+            @Override
+            public void onTripleClick() {
+//                if (prefs.lockModeOn) lockPhone();
+                super.onTripleClick();
+            }
+        };
+    }
+
     static class AppModel {
         String appLabel;
         String appPackage;
@@ -67,7 +161,7 @@ public class MainActivity extends Activity {
         }
     }
 
-    public static class AppAdapter extends ArrayAdapter<AppModel> {
+    static class AppAdapter extends ArrayAdapter<AppModel> {
 
         private static class ViewHolder {
             TextView appName;
---
+++ app/src/main/java/app/olauncher/light/OnSwipeTouchListener.java
@@ -0,0 +1,177 @@
+package app.olauncher.light;
+
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+import static java.lang.Math.abs;
+
+public class OnSwipeTouchListener implements View.OnTouchListener {
+
+    private GestureDetector gestureDetector;
+
+    public OnSwipeTouchListener(Context context) {
+        gestureDetector = new GestureDetector(context, new GestureListener());
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return gestureDetector.onTouchEvent(motionEvent);
+    }
+
+    private class GestureListener extends GestureDetector.SimpleOnGestureListener {
+        private final int SWIPE_THRESHOLD = 100;
+        private final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+        @Override
+        public boolean onDown(MotionEvent motionEvent) {
+            return true;
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent motionEvent) {
+            return super.onSingleTapUp(motionEvent);
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent motionEvent) {
+            return super.onDoubleTap(motionEvent);
+        }
+
+        @Override
+        public void onLongPress(MotionEvent motionEvent) {
+            super.onLongPress(motionEvent);
+        }
+
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY) {
+            try {
+                float diffY = event2.getY() - event1.getY();
+                float diffX = event2.getX() - event1.getX();
+                if (abs(diffX) > abs(diffY)) {
+                    if (abs(diffX) > SWIPE_THRESHOLD && abs(velocityX) > SWIPE_VELOCITY_THRESHOLD)
+                        if (diffX > 0) onSwipeRight();
+                        else onSwipeLeft();
+                } else {
+                    if (abs(diffY) > SWIPE_THRESHOLD && abs(velocityY) > SWIPE_VELOCITY_THRESHOLD)
+                        if (diffY < 0) onSwipeUp();
+                        else onSwipeDown();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+    }
+
+    public void onSwipeRight() {
+    }
+
+    public void onSwipeLeft() {
+    }
+
+    public void onSwipeUp() {
+    }
+
+    public void onSwipeDown() {
+    }
+
+    public void onLongClick() {
+    }
+
+    public void onDoubleClick() {
+    }
+
+    public void onTripleClick() {
+    }
+
+    public void onClick() {
+    }
+
+}
+
+//internal open class OnSwipeTouchListener(c: Context?) : OnTouchListener {
+//        private var longPressOn = false
+//        private var doubleTapOn = false
+//        private val gestureDetector: GestureDetector
+//
+//        override fun onTouch(view: View, motionEvent: MotionEvent): Boolean {
+//        if (motionEvent.action == MotionEvent.ACTION_UP)
+//        longPressOn = false
+//        return gestureDetector.onTouchEvent(motionEvent)
+//        }
+//
+//        private inner class GestureListener : SimpleOnGestureListener() {
+//        private val SWIPE_THRESHOLD: Int = 100
+//        private val SWIPE_VELOCITY_THRESHOLD: Int = 100
+//
+//        override fun onDown(e: MotionEvent): Boolean {
+//        return true
+//        }
+//
+//        override fun onSingleTapUp(e: MotionEvent): Boolean {
+//        if (doubleTapOn) {
+//        doubleTapOn = false
+//        onTripleClick()
+//        }
+//        return super.onSingleTapUp(e)
+//        }
+//
+//        override fun onDoubleTap(e: MotionEvent): Boolean {
+//        doubleTapOn = true
+//        Timer().schedule(Constants.TRIPLE_TAP_DELAY_MS.toLong()) {
+//        if (doubleTapOn) {
+//        doubleTapOn = false
+//        onDoubleClick()
+//        }
+//        }
+//        return super.onDoubleTap(e)
+//        }
+//
+//        override fun onLongPress(e: MotionEvent) {
+//        longPressOn = true
+//        Timer().schedule(Constants.LONG_PRESS_DELAY_MS.toLong()) {
+//        if (longPressOn) onLongClick()
+//        }
+//        super.onLongPress(e)
+//        }
+//
+//        override fun onFling(
+//        event1: MotionEvent,
+//        event2: MotionEvent,
+//        velocityX: Float,
+//        velocityY: Float
+//        ): Boolean {
+//        try {
+//        val diffY = event2.y - event1.y
+//        val diffX = event2.x - event1.x
+//        if (abs(diffX) > abs(diffY)) {
+//        if (abs(diffX) > SWIPE_THRESHOLD && abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+//        if (diffX > 0) onSwipeRight() else onSwipeLeft()
+//        }
+//        } else {
+//        if (abs(diffY) > SWIPE_THRESHOLD && abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+//        if (diffY < 0) onSwipeUp() else onSwipeDown()
+//        }
+//        }
+//        } catch (exception: Exception) {
+//        exception.printStackTrace()
+//        }
+//        return false
+//        }
+//        }
+//
+//        open fun onSwipeRight() {}
+//        open fun onSwipeLeft() {}
+//        open fun onSwipeUp() {}
+//        open fun onSwipeDown() {}
+//        open fun onLongClick() {}
+//        open fun onDoubleClick() {}
+//        open fun onTripleClick() {}
+//        private fun onClick() {}
+//
+//        init {
+//        gestureDetector = GestureDetector(c, GestureListener())
+//        }
+//        }
